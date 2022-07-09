80e7ef5b43c4d2c636a36f376101a2b8bda1e1e7
cyh92 <cyh92@live.cn>
2022-07-09 10:35:14 +0800

    Update diy-part2.sh
    # sed -i "/.login-page {/i\\
    .login-container {\n\
      margin: auto;\n\
      height: 420px\!important;\n\
      min-height: 420px\!important;\n\
      left: 0;\n\
      right: 0;\n\
      bottom: 0;\n\
      margin-left: auto\!important;\n\
      border-radius: 15px;\n\
      width: 350px\!important;\n\
    }\n\
    .login-form {\n\
      background-color: rgba(255, 255, 255, 0)\!important;\n\
      border-radius: 15px;\n\
    }\n\
    .login-form .brand {\n\
      margin: 15px auto\!important;\n\
    }\n\
    .login-form .form-login {\n\
        padding: 10px 50px\!important;\n\
    }\n\
    .login-form .errorbox {\n\
      padding: 10px\!important;\n\
    }\n\
    .login-form .cbi-button-apply {\n\
      margin: 15px auto\!important;\n\
    }\n\
    .input-group {\n\
      margin-bottom: 1rem\!important;\n\
    }\n\
    .input-group input {\n\
      margin-bottom: 0\!important;\n\
    }\n\
    .ftc {\n\
      bottom: 0\!important;\n\
    }" feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
    # sed -i "s/margin-left: 0rem \!important;/margin-left: auto\!important;/g" feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,41 +16,4 @@
 date=`date +%Y-%m-%d.%H:%M`
 sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
 # 调整argon登录框为居中
-# sed -i "/.login-page {/i\\
-.login-container {\n\
-  margin: auto;\n\
-  height: 420px\!important;\n\
-  min-height: 420px\!important;\n\
-  left: 0;\n\
-  right: 0;\n\
-  bottom: 0;\n\
-  margin-left: auto\!important;\n\
-  border-radius: 15px;\n\
-  width: 350px\!important;\n\
-}\n\
-.login-form {\n\
-  background-color: rgba(255, 255, 255, 0)\!important;\n\
-  border-radius: 15px;\n\
-}\n\
-.login-form .brand {\n\
-  margin: 15px auto\!important;\n\
-}\n\
-.login-form .form-login {\n\
-    padding: 10px 50px\!important;\n\
-}\n\
-.login-form .errorbox {\n\
-  padding: 10px\!important;\n\
-}\n\
-.login-form .cbi-button-apply {\n\
-  margin: 15px auto\!important;\n\
-}\n\
-.input-group {\n\
-  margin-bottom: 1rem\!important;\n\
-}\n\
-.input-group input {\n\
-  margin-bottom: 0\!important;\n\
-}\n\
-.ftc {\n\
-  bottom: 0\!important;\n\
-}" feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-# sed -i "s/margin-left: 0rem \!important;/margin-left: auto\!important;/g" feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+

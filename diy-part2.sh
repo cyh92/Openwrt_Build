5d943f263dd4dcb42548a7b074fd2c21c050caa2
cyh92 <cyh92@live.cn>
2022-08-13 14:18:22 +0800

    优化版本号显示

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,6 @@
 #设置版本为当前时间
 # date=`date +%Y-%m-%d.%H:%M`
 #date=`date +%m.%d.%Y`
-date=`date +%Y.%m.%d`
-sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
-
+date=`date +%Y.%m.%d_%H%M%S`
+#sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
+sed -i "s/OpenWrt /v$date\/ /g" openwrt/package/lean/default-settings/files/zzz-default-settings
